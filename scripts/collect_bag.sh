3a8db4124725533682a1eab0bbd1d370251ca78d
Gabe <earlegabriel1@gmail.com>
2021-05-05 13:27:16 -0400

    multi-camera debugging

--- scripts/collect_bag.sh
+++ scripts/collect_bag.sh
@@ -10,5 +10,9 @@ echo "$bagDir/ig_handle_scan_$now.bag"
 rosbag record -O $bagDir/ig_handle_scan_$now.bag \
 /F1/image_raw/stamped \
 /F1/cam_time \
+/F2/image_raw/stamped \
+/F2/cam_time \
+/F3/image_raw/stamped \
+/F3/cam_time \
 /imu/data/stamped \
-/imu/imu_time
+/imu/imu_time \
