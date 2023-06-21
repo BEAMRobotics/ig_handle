e48f628aa0c83e2de56677097b8471345b26f343
adthoms <adthoms@uwaterloo.ca>
2023-06-20 17:03:05 -0700

    update record scripts for compressed images

--- scripts/record_bag_vicon_calib.sh
+++ scripts/record_bag_vicon_calib.sh
@@ -9,21 +9,18 @@ echo "Collecting ig_handle bag file..."
 echo "Saving to: "
 echo "$bagDir/raw.bag"
 rosbag record -O $bagDir/raw.bag \
-/F1/image_raw \
-/F1/cam_time \
-/F2/image_raw  \
-/F2/cam_time \
-/F3/image_raw  \
-/F3/cam_time \
-/F4/image_raw  \
-/F4/cam_time \
-/lidar_h/velodyne_packets \
-/lidar_h/velodyne_points \
-/lidar_v/velodyne_packets \
-/lidar_v/velodyne_points \
-/tf \
-/vicon/SDICCheckerBoardTarget/SDICCheckerBoardTarget \
-/vicon/SDICCylinderTarget1/SDICCylinderTarget1 \
-/vicon/SDICCylinderTarget2/SDICCylinderTarget2 \
-/vicon/SDICIG2/SDICIG2 \
-/vicon/markers
+  /F1/image_raw/compressed \
+  /F2/image_raw/compressed \
+  /F3/image_raw/compressed \
+  /F4/image_raw/compressed \
+  /cam/time \
+  /lidar_h/velodyne_packets \
+  /lidar_h/velodyne_points \
+  /lidar_v/velodyne_packets \
+  /lidar_v/velodyne_points \
+  /tf \
+  /vicon/SDICCheckerBoardTarget/SDICCheckerBoardTarget \
+  /vicon/SDICCylinderTarget1/SDICCylinderTarget1 \
+  /vicon/SDICCylinderTarget2/SDICCylinderTarget2 \
+  /vicon/SDICIG2/SDICIG2 \
+  /vicon/markers
