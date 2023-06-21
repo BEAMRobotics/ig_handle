19e2536fcfe112bba7a415523837808751fbdd44
adthoms <adthoms@uwaterloo.ca>
2023-06-21 16:44:50 -0700

    fix stamp_from_serialization.py

--- scripts/processing/stamp_from_serialization.py
+++ scripts/processing/stamp_from_serialization.py
@@ -1,40 +1,45 @@
 import rosbag
+import rospy
 import sys
 import argparse
 import os
 
-from utils import *
+# delay (in sec) in sending NMEA message as per /main/main.ino
+nmea_delay = rospy.Duration.from_sec(0.055)
+
 
 def stamp_from_serialization(bag, outbag, topics):
     for topic, msg, t in bag.read_messages():
         if topic in topics:
-            msg.header.stamp = t
-            # preserve unspecified topics
-            outbag.write(topic, msg, t)
+            msg.header.stamp = t - nmea_delay
+            outbag.write(topic, msg, t - nmea_delay)
         else:
+            # preserve unspecified topics
             outbag.write(topic, msg, t)
 
     return outbag
-    
-        
+
+
 def main(args):
     parser = argparse.ArgumentParser(
-        description='Restamp given topics with serialization time to repair synchronization bugs. Unspecified topics are preserved. WARNING: This should only be used if you cannot use process_raw_bag.py')
+        description='Restamp LiDAR topics with serialization time adjusted for NMEA delay. All other topics are preserved.')
     parser.add_argument('-b', '--bag', help='input bag file', required=True)
     parser.add_argument(
-        '-t', '--topics', nargs='+', help='whitespace separated list of topics', required=True)
+        '-t', '--topics', nargs='+', help='whitespace separated list of LiDar topics', default=["/lidar_h/velodyne_packets", "/lidar_h/velodyne_points", "/lidar_v/velodyne_packets", "/lidar_v/velodyne_points"])
 
     args = parser.parse_args()
 
-    topics = args.topics
-  
     bag = rosbag.Bag(args.bag)
     folder = os.path.dirname(args.bag)
-    outfile = os.path.join(folder, "output.bag")
+    outfile = os.path.join(folder, "output_softsynch.bag")
     outbag = rosbag.Bag(outfile, "w")
 
+    topics = args.topics
+
+    # restamp LiDAR messages using serialization time
     outbag = stamp_from_serialization(bag, outbag, topics)
     outbag.close()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
